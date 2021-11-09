c95c6b105a280f2ec1a693eeb914bdf535f639e0
Enmanuel Minaya <enmanuelminaya@gmail.com>
2021-11-08 20:05:13 -0400

    adding user prompt for decimal number to convert

--- convert.js
+++ convert.js
@@ -1,4 +1,21 @@
 
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Ingrese un número decimal ", function (num) {
+    const romano = convert(num);
+    console.log(`el numero romano es ${romano}`)
+    rl.close();
+});
+
+rl.on("close", function () {
+    console.log("\nBYE");
+    process.exit(0);
+});
+
 function convert(num) {
     var c = [
         ['', "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
@@ -6,10 +23,8 @@ function convert(num) {
         ['', "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
         ['', "M", "MM", "MMM"]
     ];
-    // JavaScript% / El cálculo tiene decimales, debe usar Math.floor para redondear hacia abajo
     return c[3][Math.floor(num / 1000 % 10)] + c[2][Math.floor(num / 100 % 10)] + c[1][Math.floor(num / 10 % 10)] + c[0][Math.floor(num % 10)];
 }
 
-convert(36);
 
 module.exports = convert;
